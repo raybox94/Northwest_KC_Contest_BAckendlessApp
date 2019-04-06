e10345349f81f653fb26ea52a7caa8cb2393fb51
Sambi Reddy Ch <chsr222@gmail.com>
2019-04-05 21:45:26 -0500

    Initial Commit

---
+++ Northwest_KC_Contest_BAckendlessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Northwest_KC_Contest_BAckendlessApp
+//
+//  Created by student on 4/5/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
